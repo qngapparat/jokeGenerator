09ff3ab7f517403ce01f5bfbeecef981dd2afdeb
qngapparat <jakob.wallnoefer@outlook.com>
2017-05-29 18:01:36 +0200

    improved structure (added function extractJoke())

--- src/URLJoke.java
+++ src/URLJoke.java
@@ -47,31 +47,10 @@ public class URLJoke {
 
        try {
 
-           //create a new scanner retrieving data from a URL
-           URL myURL = new URL(urlString);
-           Scanner myScanner = new Scanner(myURL.openStream());
-
-           //scan and append everything from the URL stream to a string
-           String fetchedString = "";
-           while(myScanner.hasNext()){
-               fetchedString += myScanner.next();
-               fetchedString += " ";
-           }
-           myScanner.close();
-
-           //create a Json object from the string
-           JSONObject myObj = new JSONObject(fetchedString);
-
-           //return in case "type" is not "success" (see icndb.com/api for more info)
-           if(! myObj.getString("type").equals("success")){
-               return null;
-           }
-
-           //extract "joke" as String from the retrieved JSON object
-           String joke = myObj.getJSONObject("value").getString("joke");
-
-           //return joke
-           return joke;
+           //reads json from URL into String
+           String rawJSON = readURL(urlString);
+           //extracts "joke" String from the raw json String.
+           String joke = extractJoke(rawJSON);
        }
 
        catch(MalformedURLException e){
@@ -84,11 +63,43 @@ public class URLJoke {
 
     }
 
-    public static void main(String[] args) throws Exception {
+    public String extractJoke(String rawJSON){
+
+        //create a Json object from the string
+        JSONObject myObj = new JSONObject(rawJSON);
+
+        //return in case "type" is not "success" (see icndb.com/api for more info)
+        if(! myObj.getString("type").equals("success")){
+            return null;
+        }
+
+        //extract "joke" as String from the retrieved JSON object
+        String joke = myObj.getJSONObject("value").getString("joke");
+
+        //return joke
+        return joke;
+    }
+
+    /*public static void main(String[] args) throws Exception {
 
         URLJoke myJoke = new URLJoke();
         System.out.println(myJoke.loadJokeFromURL("http://api.icndb.com/jokes/89"));
+    }*/
+
+    public String randomJoke() throws Exception{
+
+        try{
+            String rawJSON = readURL("http://api.icndb.com/jokes/random");
+            String joke = extractJoke(rawJSON);
+            return joke;
+        }
+
+        catch(Exception e){
+            throw new Exception(e);
+        }
     }
+
+
 
 
 }
