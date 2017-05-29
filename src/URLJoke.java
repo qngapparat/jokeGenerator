4bb939348072a4ed1b0a19b10257e5c216c7974b
qngapparat <jakob.wallnoefer@outlook.com>
2017-05-29 17:22:25 +0200

    actually making progress (2a,b)

--- src/URLJoke.java
+++ src/URLJoke.java
@@ -10,32 +10,44 @@ import org.json.*;
  */
 public class URLJoke {
 
-    public String readURL(String urlString) throws Exception{
+    public String readURL(String urlString) throws NoJokeFoundException{
 
        try {
+
+           //create a new scanner retrieving data from a URL
            URL myURL = new URL(urlString);
            Scanner myScanner = new Scanner(myURL.openStream());
 
+           //scan and append everything from the URL stream to a string
            String fetchedString = "";
            while(myScanner.hasNext()){
                fetchedString += myScanner.next();
            }
            myScanner.close();
 
+           //create a Json object from the string
            JSONObject myObj = new JSONObject(fetchedString);
 
-           //return in case "type" is not "success"
+           //return in case "type" is not "success" (see icndb.com/api for more info)
            if(! myObj.getString("type").equals("success")){
                return null;
            }
-            String joke = myObj.getJSONObject("value").getString("joke");
 
-           
+           //extract "joke" as String from the retrieved JSON object
+           String joke = myObj.getJSONObject("value").getString("joke");
+
+           //return joke
+           return joke;
        }
 
        catch(MalformedURLException e){
            throw new Exception(e);
        }
+
+    }
+
+    public String loadJokeFromURL(String urlString){
+
 
     }
 }
